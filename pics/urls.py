5bc857871fa45fe0a2879cc67d0a847f99d73e68
stevekibe <stevekibe2345@gmail.com>
2018-10-01 18:44:43 +0300

    deploy changes in urls

--- pics/urls.py
+++ pics/urls.py
@@ -7,9 +7,6 @@ urlpatterns=[
     url('^$',views.all_pics,name='picsToday'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^picture/(\d+)',views.picture,name ='picture'),
-    url(r'location/(\d+)',views.filter_by_location,name ='location'),
-    url(r'category/(\d+)',views.filter_by_category,name ='category'),
- 
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
