c5d94e072af7765bf11a6b444a63e8728dae64aa
stevekibe <stevekibe2345@gmail.com>
2018-10-01 19:27:45 +0300

    add the migrations

--- pics/urls.py
+++ pics/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns=[
     url('^$',views.all_pics,name='picsToday'),
     url(r'^search/', views.search_results, name='search_results'),
-    #url(r'^picture/(\d+)',views.picture,name ='picture'),
+    #url(r'^picture/()',views.picture,name ='picture'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
